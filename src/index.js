80c6662da4b0cb5066b2c69d8dcb26c1492971bd
rpire <rubenpire7@gmail.com>
2021-09-28 19:56:12 -0400

    Configure Redux store, constants, and reducers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import Bookstore from './components/Bookstore';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
-      <Bookstore />
+      <Provider store={store}>
+        <Bookstore />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import booksReducer from './books/books';
+
+const reducers = combineReducers({
+  bookList: booksReducer,
+  //Additional reducer will come here.
+});
+
+const store = createStore(
+  reducers,
+  applyMiddleware(logger),
+);
+
+export default store;
