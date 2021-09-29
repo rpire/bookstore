34c8e593af589018b94727d541081410611347a2
rpire <rubenpire7@gmail.com>
2021-09-28 20:17:52 -0400

    Change import order in index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import Bookstore from './components/Bookstore';
-import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 
 ReactDOM.render(
